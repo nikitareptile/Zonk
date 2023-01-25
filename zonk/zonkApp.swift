8cab5897f5df9babb94586fd729151584e91eae8
Nikita Tikhonyuk <nikitatikhonyuk@yandex.com>
2023-01-25 23:31:45 +1000

    Initial Commit

---
+++ zonk/zonkApp.swift
@@ -0,0 +1,17 @@
+//
+//  zonkApp.swift
+//  zonk
+//
+//  Created by Никита Тихонюк on 25.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct zonkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
